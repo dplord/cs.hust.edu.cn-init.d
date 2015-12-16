e4db6c21d9eb81793b63154c141edb046515f92f
dplord <dp199263@gmail.com>
2015-12-16 20:44:11 +0800

    导入excel表格到数据库中的脚本

---
+++ export_Excel_to_db.rb
@@ -0,0 +1,30 @@
+#encoding:utf-8
+require "rubygems"
+require "spreadsheet"
+require 'mysql'
+
+
+Spreadsheet.client_encoding = "UTF-8"
+
+book = Spreadsheet.open("1.xls")
+sheet1 = book.worksheet(0)
+#row = sheet1.row(0)
+#print row
+
+
+
+#print $levels,"\n\n",$levels.uniq
+
+m=Mysql.new("","","","")
+$sql=[]
+for i in 0..(sheet1.count-1)
+  if sheet1.row(i)!=nil
+    $sql<<="(3,'"+sheet1.row(i)[1].to_s.strip+"','"+sheet1.row(i)[2].to_s.strip+"','"+sheet1.row(i)[3].to_s.strip+"','"+sheet1.row(i)[4].to_s.strip+"','"+sheet1.row(i)[5].to_s.strip+"')"
+  end
+end
+
+sql="insert into bglike (type,cs1,cs2,cs3,cs4,cs5) values "+$sql.join(",")
+
+p sql
+m.query(sql)
+
